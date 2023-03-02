5944ae3ffe08e023298a9849a08022a9c334a418
jimohbasty <jimohabdulbasit@gmail.com>
2023-03-02 16:24:53 +0100

    pointers_arrays_strings

--- 0x06-pointers_arrays_strings/_putchar.c
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -4,6 +4,7 @@
  * _putchar - writes the character c to stdout
  * @c: char to be printed
  * Return: 1 on success
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
