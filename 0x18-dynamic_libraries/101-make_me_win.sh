b8f5b88feafd92321db20243076fb6428489f632
Abdulbasit Jimoh <jimohabdulbasit@gmail.com>
2023-08-21 13:53:19 -0700

    dynamic_libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -p /tmp https://github.com/Jimohbasty/alx/raw/main/0x18%20C%20-%20Dynamic%20libraries/libgiga.s
+wget -P /tmp https://github.com/Jimohbasty/alx/raw/main/0x18%20C%20-%20Dynamic%20libraries/libgiga.so
 export LD_PRELOAD=/tmp/libgiga.so
