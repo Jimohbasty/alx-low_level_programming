71532a76a31063a97f25e0c1cb211eac0b373c5c
Abdulbasit Jimoh <jimohabdulbasit@gmail.com>
2023-08-21 13:45:19 -0700

    100-operations

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,56 @@
+#include "main.h"
+
+/**
+  * add - The name of a function.
+  * @a: int.
+  * @b: int.
+  * Return: int .
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+  * sub - The name of a function.
+  * @a: int .
+  * @b: int .
+  * Return: int .
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+  * mul - The name of a function.
+  * @a: int .
+  * @b: int .
+  * Return: int .
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+  * div - The name of a function.
+  * @a: int .
+  * @b: int .
+  * Return: int .
+ */
+int di(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+  * mod - The name of a function.
+  * @a: int .
+  * @b: int .
+  * Return: int .
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
